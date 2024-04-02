6a2f2f2bf17f5311a36ad187025295ed6655c44d
Morgan Rory Didelin <rory.didelin@gmail.com>
2024-04-02 16:16:37 +1000

    Initial commit, project setup and base implementation of circle graph in MAUI

---
+++ MRQAndroid/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Maui.Controls;
+using MRQAndroid.Controls;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()    {
+
+        var circleGraph = new CircleGraph();
+        circleGraph.DataProvider = GetChartData;
+        Content = new StackLayout { Children = { circleGraph } };
+    }
+
+    public List<CircleGraphSection> GetChartData()
+    {
+        return new List<CircleGraphSection>
+    {
+        new CircleGraphSection { Name = "Section 1", Value = 25, Color = Colors.Red },
+        new CircleGraphSection { Name = "Section 2", Value = 25, Color = Colors.Green },
+        new CircleGraphSection { Name = "Section 3", Value = 25, Color = Colors.Blue },
+        new CircleGraphSection { Name = "Section 4", Value = 25, Color = Colors.Yellow },
+    };
+    }
+}
